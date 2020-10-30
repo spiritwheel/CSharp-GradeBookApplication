ca522f64ea8a2e6709f61b1020c93b13afa7c924
spiritwheel <fernandez.logan@yahoo.fr>
2020-10-30 16:24:56 +0100

    solution

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -13,26 +14,19 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-            }
-            if (20 <= averageGrade)
-            {
-                return 'A';
-            }
-            if (40 <= averageGrade && averageGrade < 20)
-            {
-                return 'B';
-            }
-            if (60 <= averageGrade && averageGrade < 40)
-            {
-                return 'C';
-            }
-            if (80 <= averageGrade && averageGrade < 60)
-            {
-                return 'D';
-            }
 
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (averageGrade >= grades[threshold - 1])
+                return 'A';
+            if (averageGrade >= grades[(threshold * 2) - 1])
+                return 'B';
+            if (averageGrade >= grades[(threshold * 3) - 1])
+                return 'C';
+            if (averageGrade >= grades[(threshold * 4) - 1])
+                return 'D';
             return 'F';
         }
     }
