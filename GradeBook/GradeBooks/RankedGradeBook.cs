9b7a17993a37e5b05be0918a184bde5de33986a6
spiritwheel <fernandez.logan@yahoo.fr>
2020-10-30 16:10:01 +0100

    fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,23 +12,23 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (averageGrade <= 5)
+            if (Students.Count < 5)
             {
-                throw new InvalidOperationException();
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            if (5 < averageGrade && averageGrade <= 20)
+            if (20 < averageGrade)
             {
                 return 'A';
             }
-            if (20 < averageGrade && averageGrade <= 40)
+            if ( 40 < averageGrade && averageGrade <= 20)
             {
                 return 'B';
             }
-            if (40 < averageGrade && averageGrade <= 60)
+            if (60 < averageGrade && averageGrade <= 40)
             {
                 return 'C';
             }
-            if (60 < averageGrade && averageGrade <= 80)
+            if (80 < averageGrade && averageGrade <= 60)
             {
                 return 'D';
             }
