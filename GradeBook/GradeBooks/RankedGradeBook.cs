184aae4b8a674ac6ee645c9ca86ec4852c743315
spiritwheel <fernandez.logan@yahoo.fr>
2020-10-30 16:34:04 +0100

    4 Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,5 +29,19 @@ namespace GradeBook.GradeBooks
                 return 'D';
             return 'F';
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+                if (Students.Count < 5)
+                    Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
