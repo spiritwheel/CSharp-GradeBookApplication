50db1913265f1ed50c57531401223724caf19112
lfernandez <logan.fernandez@sogeti.com>
2020-10-30 12:32:20 +0100

    fix

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook (string name): base(name)
         {
-            GradeBookType Type = GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
     }
 
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook (string name): base(name)
         {
-            GradeBookType Type = GradeBookType.Standard;
+            Type = GradeBookType.Standard;
         }
     }
 
